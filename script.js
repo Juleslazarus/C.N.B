9370297c75cf1692e727b8bee8ddafc3cd2e1117
Julian Naranjo R <naranjojulian702@gmail.com>
2022-10-03 23:53:21 -0700

    currently working on admin panel page

---
+++ script.js
@@ -0,0 +1,5 @@
+const adminBtn = document.querySelector(".headerBloomington");
+
+const closeOverlay = document.querySelector(".closeAminOverlayBtn");
+
+const adminPopupContainer = document.querySelector(".adminPopupContainer");
